36779cd7da71d8d739fe85868b0774edf6890d01
Anusha0828 <Anusha0828>
2026-03-07 14:52:42 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./components/Header";
+import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Navigation />
+      <h2>Welcome to my website</h2>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
