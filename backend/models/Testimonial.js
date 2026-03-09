f4726e0c84486f2e560246adc7af4f74436d18f4
Anusha0828 <kasanianusha413@gmail.com>
2026-03-09 10:46:38 +0530

    Delete backend directory

--- backend/models/Testimonial.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const testimonialSchema = new mongoose.Schema({
-  name: String,
-  message: String,
-  image: String
-});
-
-module.exports = mongoose.model("Testimonial", testimonialSchema);
--- backend/models/service.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const serviceSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  image: String
-});
-
-module.exports = mongoose.model("Service", serviceSchema);
